b02d0d2a5ef329bad8ff75058b65817109eceedc
dmelnev <dvmau@mail.ru>
2023-04-05 00:37:24 +0300

    day off

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,12 +9,41 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/", name="app_main")
+     * @Route("/", name="app_index")
      */
     public function index(): Response
     {
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+
+        ]);
+    }
+
+    /**
+     * @Route("/optic", name="app_optic")
+     */
+    public function optic(): Response
+    {
+        return $this->render('main/optic.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/tomography", name="app_tomography")
+     */
+    public function tomography(): Response
+    {
+        return $this->render('main/tomography.html.twig', [
+
+        ]);
+    }
+    /**
+     * @Route("/contacts", name="app_contacts")
+     */
+    public function contacts(): Response
+    {
+        return $this->render('main/contacts.html.twig', [
+
         ]);
     }
 }
